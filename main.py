f7decc0dde87e7cf77bcc9aa99df5759ffef2f6f
shimonoe <adriano.shimonoe@gmail.com>
2025-02-22 23:49:32 -0300

    fix: add timeouts to support rpi3 low processing power

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from telegram.ext import (
-    Application,
+    ApplicationBuilder,
     CommandHandler,
     MessageHandler,
     filters,
@@ -9,11 +9,23 @@ from src.config import Config
 import src.commands as commands
 import src.handlers as handlers
 
+from loguru import logger
+logger.add("./logs/file_{time}.log", rotation="1 MB", retention="10 days", level="DEBUG")
+
 
 if __name__ == "__main__":
     print("Starting bot...")
 
-    app = Application.builder().token(Config.TOKEN).build()
+    app = ApplicationBuilder()\
+        .token(Config.TOKEN)\
+        .connect_timeout(30)\
+        .connection_pool_size(8)\
+        .read_timeout(30)\
+        .write_timeout(30)\
+        .get_updates_read_timeout(30)\
+        .get_updates_write_timeout(30)\
+        .media_write_timeout(30)\
+        .build()
 
     # Comamands
     app.add_handler(CommandHandler("start", commands.start_command))
@@ -27,4 +39,4 @@ if __name__ == "__main__":
 
     # Start polling
     print("Bot is running...")
-    app.run_polling(poll_interval=3)
+    app.run_polling(poll_interval=3, timeout=30)
